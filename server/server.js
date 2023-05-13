7f4a6f5dcf2d824b3c9f94abb167d6b034ba6070
Steve Shumaker <steve.j.shumaker@gmail.com>
2023-05-12 22:22:14 -0500

    initd npm and installed express | also have basic calculator functionality working with ability to select operation

--- server/server.js
+++ server/server.js
@@ -1,1 +1,38 @@
-console.log('js sourced');
+// imports and declarations
+let operation = '';
+
+// calculator function
+function doMath(event) {
+    console.log('in doMath');
+    event.preventDefault();
+
+    let num1 = Number(document.getElementById('num1').value);
+    let num2 = Number(document.getElementById('num2').value);
+
+    if (operation === 'add') {
+        console.log(num1 + num2);
+    } else if (operation === 'subtract') {
+        console.log(num1 - num2);
+    } else if (operation === 'multiply') {
+        console.log(num1 * num2);
+    } else {
+        console.log(num1 / num2);
+    }
+}
+
+// math operation functions
+function passPlus() {
+    return operation =  'add';
+}
+
+function passMinus() {
+    return operation = 'subtract';
+}
+
+function passMultiply() {
+    return operation = 'multiply';
+}
+
+function passDivide() {
+    return operation = 'divide';
+}
