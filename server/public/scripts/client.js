94738cd9f40f793a4be673019daf647bf8165631
Steve Shumaker <steve.j.shumaker@gmail.com>
2023-05-16 15:14:58 -0500

    moved the calculations to the server

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -9,51 +9,24 @@ let eval = '';
 
 // calculator function / POST
 function doMath(event) {
-    // let eval = '';
-    let outputEl = document.getElementById('output');
     console.log('in doMath', operation);
     event.preventDefault();
 
     let num1 = Number(document.getElementById('num1').value);
     let num2 = Number(document.getElementById('num2').value);
 
-    // logic to handle different states of operations
-    if (operation === '') { // need to figure out how to handle this
-        num1 = 0;
-        num2 = 0;
-        eval = num1 * num2;
-        console.log(num1 * num2);
-    } else if (operation === 'add') {
-        eval = num1 + num2;
-        console.log(num1 + num2);
-        sign = '+';
-    } else if (operation === 'subtract') {
-        eval = num1 - num2;
-        console.log(num1 - num2);
-        sign = '-';
-    } else if (operation === 'multiply') {
-        eval = num1 * num2;
-        console.log(num1 * num2);
-        sign = '*';
-    } else {
-        eval = num1 / num2;
-        console.log(num1 / num2);
-        sign = '/';
-    }
-
-    outputEl.innerHTML = `<h2>${String(eval)}</h2>`;
 
     let calcToAdd = JSON.stringify({
         num1: num1,
-        operation: sign,
+        operation: operation,
         num2: num2,
-        result: eval
     });
 
     fetch('/calcs', {method: 'POST', body: calcToAdd, headers: { 'Content-Type': 'application/json' }})
     .then((response) => {
         document.querySelector('#memoryDiv').innerHTML = '';
         getCalcs();
+
     })
     .catch((error) => {
         console.log('Error: ', error);
@@ -96,11 +69,15 @@ function getCalcs() {
     })
     .then((calcs) => {
         let calcDiv = document.getElementById('memoryDiv');
+        let outputEl = document.getElementById('output');
+
+        outputEl.innerHTML = `<h2>${calcs[calcs.length-1].eval}</h2>`;
 
         for (let calc of calcs) {
-            calcDiv.innerHTML += `<p>${calc.num1} ${calc.operation} ${calc.num2} = ${calc.result}</p>`;
+            calcDiv.innerHTML += `<p>${calc.num1} ${calc.sign} ${calc.num2} = ${calc.eval}</p>`;
         }
     }).catch((error) => {
+        console.log(error);
         alert('Error!');
     });
 }
--- server/server.js
+++ server/server.js
@@ -13,7 +13,37 @@ app.get('/calcs', (req, res) => {
 
 // POST new calc
 app.post('/calcs', (req, res) => {
-    calcArray.push(req.body);
+    let dataFromClient = req.body;
+    let num1 = dataFromClient.num1;
+    let num2 = dataFromClient.num2;
+    
+    if (dataFromClient.operation === '') { // need to figure out how to handle this
+        num1 = 0;
+        num2 = 0;
+        dataFromClient.eval = num1 * num2;
+        console.log(num1 * num2);
+    } else if (dataFromClient.operation === 'add') {
+        dataFromClient.eval = num1 + num2;
+        console.log(num1 + num2);
+        dataFromClient.sign = '+';
+    } else if (dataFromClient.operation === 'subtract') {
+        dataFromClient.eval = num1 - num2;
+        console.log(num1 - num2);
+        dataFromClient.sign = '-';
+    } else if (dataFromClient.operation === 'multiply') {
+        dataFromClient.eval = num1 * num2;
+        console.log(num1 * num2);
+        dataFromClient.sign = '*';
+    } else {
+        dataFromClient.eval = num1 / num2;
+        console.log(num1 / num2);
+        dataFromClient.sign = '/';
+    }
+
+    // outputEl.innerHTML = `<h2>${String(dataFromClient.eval)}</h2>`;
+
+
+    calcArray.push(dataFromClient);
     res.sendStatus(201);
 })
 
